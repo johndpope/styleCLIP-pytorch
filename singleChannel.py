be74f6244bca8a540611cd6cc15d5bf6964ddc31
JennyJeoni <yoonkim313@kaist.ac.kr>
2021-08-02 22:44:48 +0900

    layerwise manipulation

--- singleChannel.py
+++ singleChannel.py
@@ -1,17 +1,11 @@
-# generate fs3.npy for W PC version
 import clip
 import torch
 import argparse
 import numpy as np
 import sys
-import copy
-import pickle
 from PIL import Image
-from types import SimpleNamespace
 from stylegan2.models import Generator
-from styleCLIP import encoder, decoder, MSCode, visual
-# def GetImgF(out,model,preprocess,device)
-# def GetFs(fs)
+from styleCLIP import encoder, decoder, visual
 
 # Shift images to CLIP image feature 
 def GetImgF(out, model, preprocess, device):
@@ -27,7 +21,7 @@ def GetImgF(out, model, preprocess, device):
     
     image=torch.cat(tmp)
     with torch.no_grad():
-        image_features = model.encode_image(image) # 2*num_images, 512
+        image_features = model.encode_image(image) # 2 * num_images, 512
     
     image_features1=image_features.cpu().numpy()
     image_features1=image_features1.reshape(list(imgs.shape[:2])+[512])
@@ -35,17 +29,20 @@ def GetImgF(out, model, preprocess, device):
     return image_features1
 
 def GetFs(fs):
-    # Take mean of given features 
-    # Returned output fs3 : (6048, 512)
-    print(f"fs shape {fs.shape}")
-    tmp=np.linalg.norm(fs,axis=-1)
-    print(f"tmp shape {tmp.shape}")
+    """
+
+    Args:
+        fs ([torch.Tensor]): number of manipulations (layers * comps), num_images, num_alphas, 512
+
+    Returns:
+        fs3 ([torch.Tensor]): 1, 512
+    """
+    tmp=np.linalg.norm(fs,axis=-1) 
     fs1=fs/tmp[:,:,:,None]
-    fs2=fs1[:,:,1,:]-fs1[:,:,0,:]  # 5*sigma - (-5)* sigma
+    fs2=fs1[:,:,1,:]-fs1[:,:,0,:]  # alpha*sigma - alpha* sigma
     fs3=fs2/np.linalg.norm(fs2,axis=-1)[:,:,None]
     fs3=fs3.mean(axis=1)
     fs3=fs3/np.linalg.norm(fs3,axis=-1)[:,None]
-    print(f"GetFs {fs3.shape}")
     return fs3
 
 # Load PCA basis vectors
@@ -58,10 +55,9 @@ def load_components(dump_name, inst=None):
     Z_comp = data['lat_comp']
     Z_mean = data['lat_mean']
     Z_stdev = data['lat_stdev']
-    # random_stdev_act = np.mean(data['random_stdevs'])
     n_comp = X_comp.shape[0]
     data.close()
-    return X_comp
+    
     # Transfer to GPU
     # components = SimpleNamespace(
     #     X_comp = torch.from_numpy(X_comp).cuda().float(),
@@ -74,6 +70,9 @@ def load_components(dump_name, inst=None):
     #     # latent_types = [inst.model.latent_space_name()]*n_comp,
     #     # ranges = [(0, inst.model.get_max_latents())]*n_comp,
     # )
+    return X_comp
+
+
 def w_stat(w_samples, std=True, num_samples=100000):
     w_samples = w_samples[:num_samples]
     shape = w_samples.shape[1]
@@ -90,7 +89,7 @@ if __name__ == "__main__":
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # device = torch.device("cpu")
-    print(f"Torch is Using Device {device}")
+    print(f"Torch Device: {device}")
     parser = argparse.ArgumentParser(description='Configuration for W space version of delta image in CLIP space')
     parser.add_argument('--use_w', action="store_true")
     parser.add_argument('--n_layer', type=int, default=None, help='Index of layer to manipulate')
@@ -127,8 +126,6 @@ if __name__ == "__main__":
     if args.use_w:
         all_features = []
         for layer_idx in range(generator.n_latent):
-            if args.n_layer is not None:
-                layer_idx = args.n_layer
             for pc_idx, pc in enumerate(comp):
                 pc = np.expand_dims(pc, 0) # 1, 512 --> 1, 1, 512
                 out=np.zeros((args.num_images, 2, 1024, 1024, 3), dtype='uint8')
@@ -136,37 +133,34 @@ if __name__ == "__main__":
                 for img_idx in range(args.num_images):
                     w_plus = np.tile(w_samples[img_idx], (1, 18, 1))
                     delta_w = np.zeros_like(w_plus)
-                    for alpha_idx, alpha in enumerate([50, -50]):
-                        print(i)
-
+                    for alpha_idx, alpha in enumerate([20, -20]):
+                        
                         delta_w[:, layer_idx, :] = pc
                         alpha *= w_std
                         latent = torch.Tensor(w_plus + alpha * delta_w).to(device)
                         style_space, style_names, noise_constants = encoder(generator, latent)
                         image = decoder(generator, style_space, latent, noise_constants)
-                        out[img_idx, alpha_idx, :, :, :] = visual(image, save=args.save, name=str(i))
-                        i += 1
-
+                        out[img_idx, alpha_idx, :, :, :] = visual(image, save=False, name=str(i))
+            
+                shp = list(out.shape[2:]) # 1024, 1024, 3
                 tmp = []
-                img = out.reshape([-1]+list(out.shape[2:]))
+                img = out.reshape([-1]+shp)
                 for i in range(len(img)): # 2 * 100
                     imgs=Image.fromarray(img[i])
-                    _ = visual(imgs, save=args.save, name="manipulated")
                     image = preprocess(imgs).unsqueeze(0).to(device)
                     tmp.append(image)
-                print(f"Length of tmp {len(tmp)}")
+
                 image = torch.cat(tmp)
                 with torch.no_grad():
-                    image_features = model.encode_image(image)
-
+                    image_features = model.encode_image(image) # num alpha * num images, 512
                 image_features = image_features.cpu().numpy()
-                image_features = image_features.reshape(list(out.shape[:2])+[512])
-            if args.n_layer is not None:
-                break
-            all_features.append(image_features)           
+                image_features = image_features.reshape([args.num_images, 2, 512])
+                all_features.append(image_features)   
+                print(f"Created image feature vector for {layer_idx}-th layer with {pc_idx}-th PC: Total of {len(all_features)}")
         all_features = np.array(all_features)
-        all_features = GetFs(all_features)
+        print(f"Array of features: {all_features.shape}")
         
-        np.save(args.file_path+'fs3',all_features)
+        all_features = GetFs(all_features)
+        np.save(args.file_path+'W_manip_50comps',all_features)
     else:
         sys.quit("Only W space manipulation is supported!")
